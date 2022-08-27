c8b224a2ecbef4779e2705c8b986705c33fa5a69
levangiau <giaubale321@gmail.com>
2022-08-27 19:04:26 +0700

    bổ sung morgan

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
     res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
