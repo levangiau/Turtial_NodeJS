fd885c08313ce3f5b595334830d24e7d282fe24b
levangiau <giaubale321@gmail.com>
2022-09-11 17:56:56 +0700

    Phương thức GET và POST

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ const port = 3000;
 
 // setup static file and scss
 app.use(express.static(path.join(__dirname, 'public')));
+// setup để lấy được thông tin trong body trong form method='post' của html
+app.use(express.urlencoded({
+    extended: true
+}));
+// setup để lấy thông tin bằng phương thức:XMLHttpRequest,fetch,axios,...
+app.use(express.json())
 // Http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 // Template engine defaul khi tạo
 
 // app.engine('handlebars', handlebars.engine());
@@ -28,6 +34,15 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.render('news');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
+app.post('/search', (req, res) => {
+    console.log("request", req.body)
+    res.send('');
 })
 
 
